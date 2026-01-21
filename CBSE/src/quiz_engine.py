ed3caac24151e4737dae510cdc62554e8c2359b0
Your Name <your-email@example.com>
2026-01-22 02:30:05 +0545

    today i didnt do much , i finished  a simple review system

--- CBSE/src/quiz_engine.py
+++ CBSE/src/quiz_engine.py
@@ -14,7 +14,7 @@ def load_data(path):
             return json.load(f)
         except: return []
 
-def save_stats(score, total, time_taken):
+def save_stats(score, total, time_taken,missed_ids):
     history = load_data(HISTORY_FILE)
     entry = {
         "date": datetime.now().strftime("%Y-%m-%d %H:%M"),
@@ -22,15 +22,25 @@ def save_stats(score, total, time_taken):
         "total": total,
         "percentage": round((score/total)*100, 2),
         "time_seconds": round(time_taken, 2),
-        "avg_speed_per_q": round(time_taken/total, 2)
+        "avg_speed_per_q": round(time_taken/total, 2),
+        "missed_questions":missed_ids
     }
     history.append(entry)
     with open(HISTORY_FILE, 'w', encoding='utf-8') as f:
         json.dump(history, f, indent=4)
     return history
 
-def run_quiz():
+def run_quiz(review_filter=None):
+
+    missed_ids = []
     questions = load_data(DATA_FILE)
+    if review_filter:
+        # Only keep questions whose number is in the missed list
+        questions = [q for q in questions if q['number'] in review_filter]
+        print(f"🎯 REVIEW MODE: Focusing on {len(questions)} missed items.")
+    else:
+        questions = questions
+        print(f"🚀 FULL QUIZ MODE: {len(questions)} Questions.")
     if not questions:
         print("❌ Error: No questions found.")
         return
@@ -60,11 +70,12 @@ def run_quiz():
                 score += 1
             else:
                 print(f" Wrong! Answer was {q['correct']}")
+                missed_ids.append(q['number'])
 
     # --- END TIMER ---
     end_time = time.time()
     total_time = end_time - start_time
-
+   
     # --- DISPLAY ANALYTICS ---
     print("\n" + "—"*30)
     print(f"FINISHED!")
@@ -81,7 +92,20 @@ def run_quiz():
         print("STATUS: LEARNING (Focus on concepts first, speed later.)")
     print("—"*32)
 
-    save_stats(score, total, total_time)
+    save_stats(score, total, total_time,missed_ids)
 
 if __name__ == "__main__":
-    run_quiz()
+    print("1. Full Quiz")
+    print("2. Review Mistakes from Last Session")
+    choice = input("Select (1/2): ")
+
+    if choice == '2':
+        history = load_data(HISTORY_FILE)
+        if history:
+            # Grab the missed_questions list from the most recent entry
+            last_missed = history[-1].get("missed_questions", [])
+            run_quiz(review_filter=last_missed)
+        else:
+            print("No history found. Run a full quiz first!")
+    else:
+        run_quiz()
